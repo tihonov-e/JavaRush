060759ffc3aedd3a95ab2c9c4b944b8e15cd4514
tihonov-e <tihonov.pcb@gmail.com>
2021-05-03 23:18:30 +0300

    solved task 1109

---
+++ task1109/src/com/company/Main.java
@@ -0,0 +1,21 @@
+/**
+ * В классе Outer есть внутренний (Inner) и вложенный (Nested) классы.
+ * В методе main класса Solution создай по одному объекту каждого из них.
+ *
+ * Требования:
+ * В методе main класса Solution должен быть создан объект класса Inner.
+ * В методе main класса Solution должен быть создан объект класса Nested.
+ * Класс Outer изменять нельзя.
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+
+        Outer.Nested nested = new Outer.Nested();
+    }
+}
---
+++ task1109/src/com/company/Outer.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/*
+Объекты внутренних и вложенных классов
+*/
+
+public class Outer {
+    class Inner {
+        Inner() {
+            System.out.println("Создание объекта внутреннего класса");
+        }
+    }
+    static class Nested {
+        Nested() {
+            System.out.println("Создание объекта вложенного класса");
+        }
+    }
+}
