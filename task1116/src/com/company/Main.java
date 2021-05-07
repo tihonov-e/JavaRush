d000e0b29350cf9360f27455e8b7518c9bb5656f
tihonov-e <tom-step@ya.ru>
2021-05-07 21:21:02 +0300

    level1

---
+++ task1116/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/*
+Правильный класс
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+
+}
+
+    class Cat {
+    String name = "Tom";
+    }
